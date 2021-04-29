4b9a7f562f598cf4add171c0aa3042bbf9c66436
Michael-Bryant Choa <mbchoa@gmail.com>
2021-04-29 00:13:14 -0700

    feat: convert to PWA

--- components/Layout.js
+++ components/Layout.js
@@ -7,6 +7,21 @@ const Layout = ({ children }) => (
   <div className="h-screen grid grid-rows-hamburger">
     <Head>
       <title>jotjik | Track your sessions</title>
+      <meta name="description" content="Time tracking PWA" />
+      <link rel="apple-touch-icon" href="/alarm-clock.png"></link>
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:url" content="https://jotjike.vercel.app" />
+      <meta name="twitter:title" content="jotjik | Track your sessions" />
+      <meta name="twitter:description" content="Time tracking PWA" />
+      <meta name="twitter:image" content="https://jotjik.vercel.app/alarm-clock.png" />
+      <meta name="twitter:creator" content="@mibrychoa" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="jotjike | Track your sessions" />
+      <meta property="og:description" content="Time tracking PWA" />
+      <meta property="og:site_name" content="jotjik" />
+      <meta property="og:url" content="https://https://jotjik.vercel.app" />
+      <meta property="og:image" content="https://jotjik.vercel.app/alarm-clock.png" />
     </Head>
     <AppHeader />
     <main className="flex flex-col p-4 max-w-screen-sm sm:px-0 sm:my-0 sm:mx-auto">{children}</main>
