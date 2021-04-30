3a5a0df250b72592b6990a81aafa4c2ac4b6e86e
Michael-Bryant Choa <mbchoa@gmail.com>
2021-04-30 15:32:20 -0700

    refactor+fix: simplify GoogleSignInButton, fix incorrect svg attribute name

--- components/Buttons/GoogleSignInButton.js
+++ components/Buttons/GoogleSignInButton.js
@@ -1,27 +1,14 @@
-import { useEffect, useState } from 'react';
 import Image from 'next/image';
-import { getCsrfToken } from 'next-auth/client';
+import { signIn } from 'next-auth/client';
 
-const GoogleSignInButton = ({ redirectUrl }) => {
-  const [csrfToken, setCsrfToken] = useState('');
-  useEffect(async () => {
-    const token = await getCsrfToken();
-    setCsrfToken(token);
-  }, []);
-
-  return (
-    <form className="w-full" action="/api/auth/signin/google" method="POST">
-      <input type="hidden" name="csrfToken" value={csrfToken} />
-      <input type="hidden" name="callbackUrl" value={redirectUrl} />
-      <button
-        type="submit"
-        className="w-full inline-flex items-center rounded-sm bg-pink-900 text-center"
-      >
-        <Image src="/btn_google_light_normal.svg" width={46} height={46} />
-        <span className="text-white font-medium mx-auto">Sign in with Google</span>
-      </button>
-    </form>
-  );
-};
+const GoogleSignInButton = ({ redirectUrl }) => (
+  <button
+    className="w-full inline-flex items-center rounded-sm bg-pink-900 text-center"
+    onClick={() => signIn('google', { callbackUrl: redirectUrl })}
+  >
+    <Image src="/btn_google_light_normal.svg" width={46} height={46} />
+    <span className="text-white font-medium mx-auto">Sign in with Google</span>
+  </button>
+);
 
 export default GoogleSignInButton;
