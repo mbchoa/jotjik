717d61672477afea6b1ad38a654aafb248c3b252
Michael-Bryant Choa <mbchoa@gmail.com>
2021-05-02 23:19:43 -0700

    refactor: update timestamp formatting, fix spacing

--- components/Timestamp.tsx
+++ components/Timestamp.tsx
@@ -8,14 +8,18 @@ const Timestamp: React.FC<Props> = ({ hours, minutes, seconds }) => (
   <>
     {parseInt(hours) > 0 && (
       <>
-        {hours} <span className="text-sm">h</span>
+        {hours}
+        <span className="text-sm">h</span>
       </>
     )}
+    &nbsp;
     {parseInt(minutes) > 0 && (
       <>
-        {minutes} <span className="text-sm">m</span>
+        {minutes}
+        <span className="text-sm">m</span>
       </>
     )}
+    &nbsp;
     {seconds}
     <span className="text-sm">s</span>
   </>
