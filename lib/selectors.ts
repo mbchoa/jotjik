3c62f00e046029946318ccc4b9bf04632ef5afc6
Michael-Bryant Choa <mbchoa@gmail.com>
2022-04-10 18:30:14 -0700

    fix: fixes broken date sorting on stats page

--- lib/selectors.ts
+++ lib/selectors.ts
@@ -8,12 +8,16 @@ export const sessionsByDay = (state): SessionsByDay => {
   return sessions(state).reduce((output, currSession) => {
     const key = formatISO(new Date(currSession.startedAt), { representation: 'date' });
     const sessionByDay = output[key] ?? { totalTime: 0, sessions: [] };
+    const sortedSessions: Session[] = [...sessionByDay.sessions, currSession].sort(
+      (a: Session, b: Session) => new Date(a.startedAt).valueOf() - new Date(b.startedAt).valueOf()
+    );
+
     return {
       ...output,
       [key]: {
         ...sessionByDay,
         totalTime: sessionByDay.totalTime + currSession.duration,
-        sessions: [...sessionByDay.sessions, currSession],
+        sessions: sortedSessions,
       },
     };
   }, {});
