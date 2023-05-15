b05c1e2d3a8e895b89a40082d873244ff1d8a04e
Michael-Bryant Choa <mbchoa@gmail.com>
2023-05-14 18:48:42 -0700

    fix typescript errors

--- prisma/migrate-data.ts
+++ prisma/migrate-data.ts
@@ -1,6 +1,6 @@
-import { PrismaClient } from "@prisma/client";
-import "dotenv/config";
-import { MongoClient } from "mongodb";
+import { PrismaClient } from '@prisma/client';
+import 'dotenv/config';
+import { MongoClient } from 'mongodb';
 
 const prisma = new PrismaClient();
 const mongo = new MongoClient(process.env.MONGO_DATABASE_URL!);
@@ -9,11 +9,11 @@ async function main() {
   try {
     // connect to db
     await mongo.connect();
-    console.log("Connected to database");
+    console.log('Connected to database');
 
-    const sessionsCollection = mongo.db("jotjik").collection("sessions");
-    const usersCollection = mongo.db("jotjik").collection("users");
-    const accountsCollection = mongo.db("jotjik").collection("accounts");
+    const sessionsCollection = mongo.db('jotjik').collection('sessions');
+    const usersCollection = mongo.db('jotjik').collection('users');
+    const accountsCollection = mongo.db('jotjik').collection('accounts');
     const timedSessions = await sessionsCollection.find({}).toArray();
     const users = await usersCollection.find({}).toArray();
     const accounts = await accountsCollection.find({}).toArray();
@@ -30,35 +30,38 @@ async function main() {
       });
 
       const newUsers = await tx.user.findMany({});
-      if (newUsers[0] === undefined || newUsers[1] === undefined) {
-        throw new Error("Error creating new users");
+      const userOne = newUsers[0];
+      const userTwo = newUsers[1];
+
+      if (userOne === undefined || userTwo === undefined) {
+        throw new Error('Error creating new users');
       }
 
       await tx.account.createMany({
         data: [
           {
-            type: "oauth",
-            provider: "google",
-            providerAccountId: "113708295991195694837",
+            type: 'oauth',
+            provider: 'google',
+            providerAccountId: '113708295991195694837',
             access_token:
-              "ya29.a0AWY7Cklh0SYomXtm11G03AjhrgOnfFjhhYSIWIXfS5-5jmnHSjp2kVBlFhLoeNiV4BrQcGhlMKQmPuymh4YpxDy5hKBrA4NxtF02YVAlmMCUxMwOPnnFuKX4eZ8nn6_u1HF_bJGLi1OaLIcOc_tVAtk0eIsWaCgYKAe0SARESFQG1tDrpQx7gJMsuHrS5YOoFtpjTHw0163",
+              'ya29.a0AWY7Cklh0SYomXtm11G03AjhrgOnfFjhhYSIWIXfS5-5jmnHSjp2kVBlFhLoeNiV4BrQcGhlMKQmPuymh4YpxDy5hKBrA4NxtF02YVAlmMCUxMwOPnnFuKX4eZ8nn6_u1HF_bJGLi1OaLIcOc_tVAtk0eIsWaCgYKAe0SARESFQG1tDrpQx7gJMsuHrS5YOoFtpjTHw0163',
             expires_at: null,
-            token_type: "Bearer",
+            token_type: 'Bearer',
             scope:
-              "openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile",
-            userId: newUsers[0].id,
+              'openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
+            userId: userOne.id,
           },
           {
-            type: "oauth",
-            provider: "google",
-            providerAccountId: "115578235515828145802",
+            type: 'oauth',
+            provider: 'google',
+            providerAccountId: '115578235515828145802',
             access_token:
-              "ya29.a0AfH6SMCVe0h2PJX3FjyKdPJmCwrdndyMRWvDbhu6mkWXQDucQizSTj8-jeScYe_EKuECrFpFoIAL6GG_WR_jECQ9vyDHDbYlS4-Zs_vrm2r_14UufgbydcXniSb3H1eUKHKPiBz4EbtjlZ3szAqIiVDi1CHX",
+              'ya29.a0AfH6SMCVe0h2PJX3FjyKdPJmCwrdndyMRWvDbhu6mkWXQDucQizSTj8-jeScYe_EKuECrFpFoIAL6GG_WR_jECQ9vyDHDbYlS4-Zs_vrm2r_14UufgbydcXniSb3H1eUKHKPiBz4EbtjlZ3szAqIiVDi1CHX',
             expires_at: null,
-            token_type: "Bearer",
+            token_type: 'Bearer',
             scope:
-              "openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile",
-            userId: newUsers[1].id,
+              'openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
+            userId: userTwo.id,
           },
         ],
       });
@@ -66,18 +69,18 @@ async function main() {
       await tx.timedSessions.createMany({
         data: [
           ...timedSessions
-            .filter(({ userId }) => userId === "637a895072775d000f6abb76")
+            .filter(({ userId }) => userId === '62f614bcdd66d426e4b1cb24')
             .map((timedSession) => ({
               startedAt: timedSession.startedAt,
               duration: timedSession.duration,
-              userId: newUsers[1]?.id,
+              userId: userOne.id,
             })),
           ...timedSessions
-            .filter(({ userId }) => userId === "62f614bcdd66d426e4b1cb24")
+            .filter(({ userId }) => userId === '637a895072775d000f6abb76')
             .map((timedSession) => ({
               startedAt: timedSession.startedAt,
               duration: timedSession.duration,
-              userId: newUsers[0]?.id,
+              userId: userTwo.id,
             })),
         ],
       });
@@ -87,7 +90,7 @@ async function main() {
   } catch (err: unknown) {
     console.error(err);
   } finally {
-    console.log("Closing database connection");
+    console.log('Closing database connection');
     await mongo.close();
   }
 }
