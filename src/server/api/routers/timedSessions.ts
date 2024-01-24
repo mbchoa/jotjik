d43b898b2b2698b8bcfeb016320e9d28e5f79ab1
Michael-Bryant Choa <mbchoa@gmail.com>
2024-01-23 23:08:59 -0800

    fix: update to fetch last 7 days worth of sessions

--- src/server/api/routers/timedSessions.ts
+++ src/server/api/routers/timedSessions.ts
@@ -1,32 +1,41 @@
 import { createTRPCRouter, protectedProcedure } from '@/server/api/trpc';
+import { startOfDay, subDays } from 'date-fns';
 import { z } from 'zod';
 
 export const timedSessionsRouter = createTRPCRouter({
   getInfiniteTimedSessions: protectedProcedure
     .input(
       z.object({
-        limit: z.number().min(1).max(100).nullish(),
-        cursor: z.string().nullish(),
+        cursor: z.string().nullish(), // cursor will now represent a date in ISO string format
       })
     )
     .query(async ({ input, ctx }) => {
       const { cursor } = input;
-      const limit = input.limit ?? 10;
+
+      console.log(cursor);
+      // Calculate the start date for the query
+      const startDate = cursor ? new Date(cursor) : new Date();
+      const endDate = startOfDay(subDays(startDate, 7));
+
       const timedSessions = await ctx.prisma.timedSessions.findMany({
-        take: limit + 1,
         where: {
           userId: ctx.session.user.id,
+          startedAt: {
+            gte: endDate,
+            lt: startDate,
+          },
         },
-        cursor: cursor ? { id: cursor } : undefined,
         orderBy: {
           startedAt: 'desc',
         },
       });
-      let nextCursor: typeof cursor | undefined = undefined;
-      if (timedSessions.length > limit) {
-        const nextSession = timedSessions.pop();
-        if (nextSession) {
-          nextCursor = nextSession.id;
+
+      let nextCursor: string | undefined = undefined;
+      if (timedSessions.length > 0) {
+        const earliestSession = timedSessions[timedSessions.length - 1];
+        if (earliestSession) {
+          const earliestSessionDate = earliestSession.startedAt;
+          nextCursor = startOfDay(subDays(earliestSessionDate, 1)).toISOString();
         }
       }
 
