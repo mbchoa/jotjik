885525ec408021575b6829f2de5e1a53184c7963
Michael-Bryant Choa <mbchoa@gmail.com>
2024-08-26 20:47:52 -0700

    add 30-day average

--- src/components/MetricsSection.tsx
+++ src/components/MetricsSection.tsx
@@ -2,23 +2,40 @@ import { trpc } from '@/utils/api';
 import { MetricCard } from './MetricCard';
 
 export const MetricsSection = () => {
-  const { data, isLoading, isFetched } = trpc.metrics.getTotalTimeForYear.useQuery({
+  const {
+    data: totalTimeForYear,
+    isLoading: isLoadingTotalTime,
+    isFetched: isFetchedTotalTime,
+  } = trpc.metrics.getTotalTimeForYear.useQuery({
     year: new Date().getFullYear(),
     timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
   });
 
-  const hh = data?.totalHours ?? 0;
-  const mm = data?.totalMinutes ?? 0;
+  const {
+    data: last30DayAverage,
+    isLoading: isLoadingLast30DayAverage,
+    isFetched: isFetchedLast30DayAverage,
+  } = trpc.metrics.getLast30DaySessionAverage.useQuery();
+
+  const totalHh = totalTimeForYear?.totalHours ?? 0;
+  const totalMm = totalTimeForYear?.totalMinutes ?? 0;
+  const last30DayAvgHh = last30DayAverage?.avgHours ?? 0;
+  const last30DayAvgMm = last30DayAverage?.avgMinutes ?? 0;
 
   return (
     <div className="flex gap-4">
       <MetricCard
         label="2024 Total"
-        metric={`${hh} h ${mm} m`}
-        isLoading={isLoading}
-        hasError={!isFetched && !data}
+        metric={`${totalHh} h ${totalMm} m`}
+        isLoading={isLoadingTotalTime}
+        hasError={!isFetchedTotalTime && !totalTimeForYear}
       />
-      <MetricCard label="Average" metric="1 h 15 m" isLoading={false} />
+      <MetricCard
+        label="30-Day Avg"
+        metric={`${last30DayAvgHh} h ${last30DayAvgMm} m`}
+        isLoading={isLoadingLast30DayAverage}
+        hasError={!isFetchedLast30DayAverage && !last30DayAverage}
+      />
     </div>
   );
 };
--- src/server/api/routers/metrics.ts
+++ src/server/api/routers/metrics.ts
@@ -39,4 +39,37 @@ export const metricsRouter = createTRPCRouter({
         totalSeconds: result[0]?.total_seconds ?? 0,
       };
     }),
+  getLast30DaySessionAverage: protectedProcedure.query(async ({ ctx }) => {
+    const sqlQuery = Prisma.sql`
+        WITH session_stats AS (
+          SELECT 
+              AVG(duration) AS avg_duration_ms
+          FROM 
+              "TimedSessions"
+          WHERE 
+              "userId" = ${ctx.session.user.id}
+              AND "startedAt" >= CURRENT_DATE - INTERVAL '30 days'
+      )
+      SELECT 
+          FLOOR(avg_duration_ms / 3600000) AS avg_hours,
+          FLOOR((avg_duration_ms % 3600000) / 60000) AS avg_minutes,
+          FLOOR((avg_duration_ms % 60000) / 1000) AS avg_seconds
+      FROM 
+          session_stats;
+      `;
+
+    const result = await ctx.prisma.$queryRaw<
+      { avg_hours: number; avg_minutes: number; avg_seconds: number }[]
+    >(sqlQuery);
+
+    if (result.length === 0) {
+      return { avgHours: 0, avgMinutes: 0, avgSeconds: 0 };
+    }
+
+    return {
+      avgHours: result[0]?.avg_hours ?? 0,
+      avgMinutes: result[0]?.avg_minutes ?? 0,
+      avgSeconds: result[0]?.avg_seconds ?? 0,
+    };
+  }),
 });
